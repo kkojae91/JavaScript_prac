ca81a4d8d4bcfceda9e76af02963aa3133d1a09e
kojaejeung <jaejeung3210@likelion.org>
2020-10-02 13:38:28 +0900

    js string

---
+++ js_syntax_생활코딩/string.js
@@ -0,0 +1,12 @@
+// alert("\"coding everybody\"");
+// console.log("coding everybody");
+// alert(typeof 1);
+// console.log(typeof "1");
+// alert("안녕하세요? \n생활코딩의 세계에 오신 것을 환영합니다.")
+// console.log("hi!! \nMy name is Kkojae")
+// alert("coding"+" everybody");
+// console.log("coding everybody".length)
+console.log("code".indexOf("c"));
+console.log("code".indexOf("o"));
+console.log("code".indexOf("d"));
+console.log("code".indexOf("e"));
