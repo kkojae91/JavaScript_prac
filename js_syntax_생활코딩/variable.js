a8701a09a2abcbffbc8c65f73727a1ff2aaba635
kojaejeung <jaejeung3210@likelion.org>
2020-10-02 14:10:22 +0900

    js variable

---
+++ js_syntax_생활코딩/variable.js
@@ -0,0 +1,28 @@
+// var a = 1;
+// alert(a+1);
+// var a = 2;
+// console.log(a+3);
+// var a = 1;
+// var b = 2;
+// alert(a+b);
+// a=2;
+// console.log(a+b);
+
+// var first="coding", second="everybody"
+// var first = "coding";
+// var second = "everybody";
+// alert(first+" "+second);
+// console.log(first);
+// console.log(second);
+
+// 추후 c의 값만 변경하면 다른 답을 얻을 수 있다.
+var c = 100;
+c = c + 10;
+alert(c);
+c = c / 10;
+alert(c);
+c = c - 10;
+alert(c);
+c = c * 10;
+alert(c);
+
