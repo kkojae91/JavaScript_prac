5c6ac196401c7fdc5e018893f5baf3ef95de9d53
kojaejeung <jaejeung3210@likelion.org>
2020-10-05 17:05:08 +0900

    web_js bom_LocationObject

---
+++ js_webbrowser_생활코딩/bom_LocationObject.js
@@ -0,0 +1,9 @@
+var naver2 = document.getElementById("naver2");
+naver2.addEventListener("click", function(){
+    location.href = "https://www.naver.com"
+})
+
+var reload2 = document.getElementById("reload2");
+reload2.addEventListener("click", function(){
+    location.reload();
+})
